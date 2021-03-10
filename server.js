42b91101caad24f19843e83ab1762f60121f313b
Navtosh Kumar <44619905+navtosh-kumar@users.noreply.github.com>
2021-03-10 13:59:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-// This is just a simple server
-const database = require('./database');
 const express = require('express');
 
 const app = express();
+
 // define cache
 const cache = {};
+
+const database = {
+    // we will be using this database to get this html page
+    ['index.html']: '<html>Hello Caching!</html>',
+};
+
+getDatabase = (key, callback) => {
+    // what this does is after 3000 miliseconds, calls the databse[key] 
+    setTimeout(() => {
+        callback(database[key]);
+    }, 3000);
+}
 
 // There are two end points defined
 
@@ -13,7 +24,7 @@ app.get('/nocache/index.html', (req, res) => {
     // all it does is, it calls the database
     // to get the html page that is being called
     // and once it gets the page just send the page
-    database.get('index.html', page => {
+    getDatabase('index.html', page => {
         res.send(page);
     });
 });
@@ -31,7 +42,7 @@ app.get('/withcache/index.html', (red, res) => {
     // Otherwise we make a call to the database
     // and cache the page 
     // then we return the response of the page
-    database.get('index.html', page => {
+    getDatabase('index.html', page => {
         cache['index.html'] = page;
         res.send(page);
     });
